596684956c9e200a67a6623a7940f16cac9a835e
https://github.com/bruutenbraaf/bruutenbraf.git <info@bruutenbraaf.nl>
2019-05-19 18:07:27 +0200

    Fixes

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="author" content="Convident">
     <meta name="apple-mobile-web-app-status-bar-style" content="white">
-	<meta name="theme-color" content="#ededed">
+    <meta name="theme-color" content="#ededed">
+    <meta name="51137a74975e267" content="c79e0a42f1a0a033e5590f6ea20d447d" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
     <title><?php wp_title(''); ?></title>
