11c83d979e697aa3759fbe215d13804ffb781e74
Bruut & Braaf <info@bruutenbraaf.nl>
2020-02-23 14:34:34 +0100

    Update

--- template-parts/content-vitaminens.php
+++ template-parts/content-vitaminens.php
@@ -33,61 +33,61 @@
                     <div class="col"><?php the_field('vitamine_b2_w'); ?></div>
                 </div>
             </li><?php } ?>
-        <?php if (get_field('vitamine_b3') != "-" && get_field('vitamine_b3_w') != "-") { ?><li>
+        <?php if (get_field('vitamine_b3') != "-") { ?><li>
                 <div class="row">
                     <div class="col"><span class="it-title">Vitamine B3</span><span class="it-waarde"><?php the_field('vitamine_b3'); ?></span></div>
                     <div class="col"><?php the_field('vitamine_b3_w'); ?></div>
                 </div>
             </li><?php } ?>
-        <?php if (get_field('vitamine_b5') != "-" && get_field('vitamine_b5_w') != "-") { ?><li>
+        <?php if (get_field('vitamine_b5') != "-") { ?><li>
                 <div class="row">
                     <div class="col"><span class="it-title">Vitamine B5</span><span class="it-waarde"><?php the_field('vitamine_b5'); ?></span></div>
                     <div class="col"><?php the_field('vitamine_b5_w'); ?></div>
                 </div>
             </li><?php } ?>
-        <?php if (get_field('vitamine_b6') != "-" && get_field('vitamine_b6_w') != "-") { ?><li>
+        <?php if (get_field('vitamine_b6') != "-") { ?><li>
                 <div class="row">
                     <div class="col"><span class="it-title">Vitamine B6</span><span class="it-waarde"><?php the_field('vitamine_b6'); ?></span></div>
                     <div class="col"><?php the_field('vitamine_b6_w'); ?></div>
                 </div>
             </li><?php } ?>
-        <?php if (get_field('vitamine_b8') != "-" && get_field('vitamine_b8_w') != "-") { ?><li>
+        <?php if (get_field('vitamine_b8') != "-") { ?><li>
                 <div class="row">
                     <div class="col"><span class="it-title">Vitamine B8</span><span class="it-waarde"><?php the_field('vitamine_b8'); ?></span></div>
                     <div class="col"><?php the_field('vitamine_b8_w'); ?></div>
                 </div>
             </li><?php } ?>
-        <?php if (get_field('vitamine_b11') != "-" && get_field('vitamine_b11_w') != "-") { ?><li>
+        <?php if (get_field('vitamine_b11') != "-") { ?><li>
                 <div class="row">
                     <div class="col"><span class="it-title">Vitamine B11</span><span class="it-waarde"><?php the_field('vitamine_b11'); ?></span></div>
                     <div class="col"><?php the_field('vitamine_b11_w'); ?></div>
                 </div>
             </li><?php } ?>
-        <?php if (get_field('vitamine_b12' && get_field('vitamine_b12_w') != "-") { ?><li>
+        <?php if (get_field('vitamine_b12') != "-") { ?><li>
                 <div class="row">
                     <div class="col"><span class="it-title">Vitamine B12</span><span class="it-waarde"><?php the_field('vitamine_b12'); ?></span></div>
                     <div class="col"><?php the_field('vitamine_b12_w'); ?></div>
                 </div>
             </li><?php } ?>
-        <?php if (get_field('vitamine_c') != "-" && get_field('vitamine_c_w') != "-") { ?><li>
+        <?php if (get_field('vitamine_c') != "-") { ?><li>
                 <div class="row">
                     <div class="col"><span class="it-title">Vitamine C</span><span class="it-waarde"><?php the_field('vitamine_c'); ?></span></div>
                     <div class="col"><?php the_field('vitamine_c_w'); ?></div>
                 </div>
             </li><?php } ?>
-        <?php if (get_field('vitamine_d') != "-" && get_field('vitamine_d_w') != "-") { ?><li>
+        <?php if (get_field('vitamine_d') != "-") { ?><li>
                 <div class="row">
                     <div class="col"><span class="it-title">Vitamine D</span><span class="it-waarde"><?php the_field('vitamine_d'); ?></span></div>
                     <div class="col"><?php the_field('vitamine_d_w'); ?></div>
                 </div>
             </li><?php } ?>
-        <?php if (get_field('vitamine_e') != "-" && get_field('vitamine_e_w') != "-") { ?><li>
+        <?php if (get_field('vitamine_e') != "-") { ?><li>
                 <div class="row">
                     <div class="col"><span class="it-title">Vitamine E</span><span class="it-waarde"><?php the_field('vitamine_e'); ?></span></div>
                     <div class="col"><?php the_field('vitamine_e_w'); ?></div>
                 </div>
             </li><?php } ?>
-        <?php if (get_field('vitamine_k') != "-" && get_field('vitamine_k_w') != "-") { ?><li>
+        <?php if (get_field('vitamine_k') != "-") { ?><li>
                 <div class="row">
                     <div class="col"><span class="it-title">Vitamine K</span><span class="it-waarde"><?php the_field('vitamine_k'); ?></span></div>
                     <div class="col"><?php the_field('vitamine_k_w'); ?></div>
